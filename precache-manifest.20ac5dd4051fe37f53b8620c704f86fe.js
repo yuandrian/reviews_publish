0668fd8116f52d0e8494eec1c9983a0e175230fc
yuandrian <yu.andrian@gmail.com>
2022-03-29 16:26:48 -0700

    Updates

---
+++ precache-manifest.20ac5dd4051fe37f53b8620c704f86fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120584af1cb96ac5f0e52ccdffa77733",
+    "url": "/reviews_publish/index.html"
+  },
+  {
+    "revision": "5585e89df7792e31b11c",
+    "url": "/reviews_publish/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews_publish/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews_publish/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5585e89df7792e31b11c",
+    "url": "/reviews_publish/static/js/main.83cf2df2.chunk.js"
+  },
+  {
+    "revision": "4d1f8170182ea89ba12c",
+    "url": "/reviews_publish/static/js/runtime-main.bf4a5dd2.js"
+  }
+]);
